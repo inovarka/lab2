a190fc232cc01f46e242d4ca9c93cb9418cdd9e8
Paul Tim <paveltimofeenko@gmail.com>
2020-12-23 22:16:34 +0300

    add implementation

--- go.mod
+++ go.mod
@@ -1,7 +1,11 @@
 // TODO: Rename to your own module.
-module github.com/roman-mazur/architecture-lab-2
+module github.com/inovarka/lab2
 
 go 1.15
 
-// TODO: Change if need to use a different assertions library.
-require github.com/stretchr/testify v1.6.1
+require (
+	github.com/golang-collections/collections v0.0.0-20130729185459-604e922904d3
+	github.com/roman-mazur/architecture-lab-2 v0.0.0-20201004160640-067ff913ee9f // indirect
+	// TODO: Change if need to use a different assertions library.
+	github.com/stretchr/testify v1.6.1
+)
--- implementation.go
+++ implementation.go
@@ -1,9 +1,111 @@
 package lab2
 
-import "fmt"
+import (
+	"math"
+	"errors"
+	"strconv"
+	"strings"
+)
 
-// TODO: document this function.
-// PrefixToPostfix converts
-func PrefixToPostfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+
+type expression interface {
+	Compute() float64
+}
+
+type operand struct {
+	value float64
+}
+
+func (o operand) Compute() float64 { return o.value }
+
+//constants for operations -> to string
+const (
+	MUL = iota
+	DIV = iota
+	ADD = iota
+	SUB = iota
+	POW = iota
+)
+
+type operation struct {
+	left   expression
+	right  expression
+	opType int
+}
+
+func (o operation) Compute() float64 {
+	res := 0.0
+	left := o.left.Compute()
+	right := o.right.Compute()
+
+	switch o.opType {
+	case ADD:
+		res = left + right
+	case SUB:
+		res = left - right
+	case MUL:
+		res = left * right
+	case DIV:
+		res = left / right
+	case POW:
+		res = math.Pow(left, right)
+	}
+
+	return res
+}
+
+func parseOp(opType int, exp []string) (expression, error, []string) {
+	left, err1, rest1 := parse(exp)
+	if err1 != nil {
+		return nil, err1, nil
+	} else if len(rest1) <= 0 {
+		return nil, errors.New("syntax error"), nil
+	}
+
+	right, err2, rest2 := parse(rest1)
+	if err2 != nil {
+		return nil, err2, nil
+	}
+
+	return operation{
+		left:   left,
+		right:  right,
+		opType: opType,
+	}, nil, rest2
+}
+
+func parse(input[] string) (expression, error, []string) {
+	if len(input) <= 0 {
+		return nil, errors.New("syntax error"), nil
+	}
+
+	switch input[0] {
+	case "*":
+		return parseOp(MUL, input[1:])
+	case "/":
+		return parseOp(DIV, input[1:])
+	case "+":
+		return parseOp(ADD, input[1:])
+	case "-":
+		return parseOp(SUB, input[1:])
+	case "^":
+		return parseOp(POW, input[1:])
+	default:
+		num, err := strconv.ParseFloat(input[0], 64)
+		if err != nil {
+			return nil, err, nil
+		}
+		return operand{value: num}, nil, input[1:]
+	}
+}
+
+func ComputePrefix(input string) (float64, error) {
+	expression, err, rest := parse(strings.Split(input, " "))
+	if len(rest) > 0 {
+		return 0, errors.New("redundant characters")
+	} else if err != nil {
+		return 0, err
+	} else {
+		return expression.Compute(), nil
+	}
 }
